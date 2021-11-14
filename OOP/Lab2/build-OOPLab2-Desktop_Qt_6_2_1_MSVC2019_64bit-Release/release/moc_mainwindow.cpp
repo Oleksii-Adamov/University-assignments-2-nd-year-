f9990ce31ae22fe18d74d73e3a8d2fa9d4d9dbe7
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2021-11-14 17:02:53 +0200

    Added release build

---
+++ OOP/Lab2/build-OOPLab2-Desktop_Qt_6_2_1_MSVC2019_64bit-Release/release/moc_mainwindow.cpp
@@ -0,0 +1,150 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../OOPLab2/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    const uint offsetsAndSize[24];
+    char stringdata0[178];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 10), // "MainWindow"
+QT_MOC_LITERAL(11, 14), // "create_project"
+QT_MOC_LITERAL(26, 0), // ""
+QT_MOC_LITERAL(27, 9), // "file_name"
+QT_MOC_LITERAL(37, 21), // "delete_project_button"
+QT_MOC_LITERAL(59, 4), // "name"
+QT_MOC_LITERAL(64, 19), // "edit_project_button"
+QT_MOC_LITERAL(84, 8), // "old_name"
+QT_MOC_LITERAL(93, 8), // "new_name"
+QT_MOC_LITERAL(102, 24), // "on_projectButton_clicked"
+QT_MOC_LITERAL(127, 22), // "on_actionNew_triggered"
+QT_MOC_LITERAL(150, 27) // "on_actionSettings_triggered"
+
+    },
+    "MainWindow\0create_project\0\0file_name\0"
+    "delete_project_button\0name\0"
+    "edit_project_button\0old_name\0new_name\0"
+    "on_projectButton_clicked\0"
+    "on_actionNew_triggered\0"
+    "on_actionSettings_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   50,    2, 0x0a,    1 /* Public */,
+       4,    1,   53,    2, 0x0a,    3 /* Public */,
+       6,    2,   56,    2, 0x0a,    5 /* Public */,
+       9,    0,   61,    2, 0x08,    8 /* Private */,
+      10,    0,   62,    2, 0x08,    9 /* Private */,
+      11,    0,   63,    2, 0x08,   10 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->create_project((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->delete_project_button((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 2: _t->edit_project_button((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 3: _t->on_projectButton_clicked(); break;
+        case 4: _t->on_actionNew_triggered(); break;
+        case 5: _t->on_actionSettings_triggered(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSize,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
+, QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 6;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ OOP/Lab2/build-OOPLab2-Desktop_Qt_6_2_1_MSVC2019_64bit-Release/release/moc_todolistwindow.cpp
@@ -0,0 +1,223 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'todolistwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../OOPLab2/todolistwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QScreen>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'todolistwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ToDoListWindow_t {
+    const uint offsetsAndSize[38];
+    char stringdata0[395];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_ToDoListWindow_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_ToDoListWindow_t qt_meta_stringdata_ToDoListWindow = {
+    {
+QT_MOC_LITERAL(0, 14), // "ToDoListWindow"
+QT_MOC_LITERAL(15, 21), // "delete_project_button"
+QT_MOC_LITERAL(37, 0), // ""
+QT_MOC_LITERAL(38, 4), // "name"
+QT_MOC_LITERAL(43, 19), // "edit_project_button"
+QT_MOC_LITERAL(63, 7), // "oldname"
+QT_MOC_LITERAL(71, 8), // "new_name"
+QT_MOC_LITERAL(80, 12), // "edit_project"
+QT_MOC_LITERAL(93, 19), // "increment_pomodoros"
+QT_MOC_LITERAL(113, 11), // "update_list"
+QT_MOC_LITERAL(125, 23), // "on_actionBack_triggered"
+QT_MOC_LITERAL(149, 24), // "on_pushButtonAdd_clicked"
+QT_MOC_LITERAL(174, 31), // "on_pushButtonStartTimer_clicked"
+QT_MOC_LITERAL(206, 25), // "on_pushButtonEdit_clicked"
+QT_MOC_LITERAL(232, 27), // "on_pushButtonDelete_clicked"
+QT_MOC_LITERAL(260, 38), // "on_actionDelete_this_project_..."
+QT_MOC_LITERAL(299, 30), // "on_actionEditProject_triggered"
+QT_MOC_LITERAL(330, 36), // "on_pushButton_task_completed_..."
+QT_MOC_LITERAL(367, 27) // "on_actionSettings_triggered"
+
+    },
+    "ToDoListWindow\0delete_project_button\0"
+    "\0name\0edit_project_button\0oldname\0"
+    "new_name\0edit_project\0increment_pomodoros\0"
+    "update_list\0on_actionBack_triggered\0"
+    "on_pushButtonAdd_clicked\0"
+    "on_pushButtonStartTimer_clicked\0"
+    "on_pushButtonEdit_clicked\0"
+    "on_pushButtonDelete_clicked\0"
+    "on_actionDelete_this_project_triggered\0"
+    "on_actionEditProject_triggered\0"
+    "on_pushButton_task_completed_clicked\0"
+    "on_actionSettings_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ToDoListWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      14,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   98,    2, 0x06,    1 /* Public */,
+       4,    2,  101,    2, 0x06,    3 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       7,    1,  106,    2, 0x0a,    6 /* Public */,
+       8,    0,  109,    2, 0x0a,    8 /* Public */,
+       9,    0,  110,    2, 0x0a,    9 /* Public */,
+      10,    0,  111,    2, 0x08,   10 /* Private */,
+      11,    0,  112,    2, 0x08,   11 /* Private */,
+      12,    0,  113,    2, 0x08,   12 /* Private */,
+      13,    0,  114,    2, 0x08,   13 /* Private */,
+      14,    0,  115,    2, 0x08,   14 /* Private */,
+      15,    0,  116,    2, 0x08,   15 /* Private */,
+      16,    0,  117,    2, 0x08,   16 /* Private */,
+      17,    0,  118,    2, 0x08,   17 /* Private */,
+      18,    0,  119,    2, 0x08,   18 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    5,    6,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ToDoListWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ToDoListWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->delete_project_button((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 1: _t->edit_project_button((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
+        case 2: _t->edit_project((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 3: _t->increment_pomodoros(); break;
+        case 4: _t->update_list(); break;
+        case 5: _t->on_actionBack_triggered(); break;
+        case 6: _t->on_pushButtonAdd_clicked(); break;
+        case 7: _t->on_pushButtonStartTimer_clicked(); break;
+        case 8: _t->on_pushButtonEdit_clicked(); break;
+        case 9: _t->on_pushButtonDelete_clicked(); break;
+        case 10: _t->on_actionDelete_this_project_triggered(); break;
+        case 11: _t->on_actionEditProject_triggered(); break;
+        case 12: _t->on_pushButton_task_completed_clicked(); break;
+        case 13: _t->on_actionSettings_triggered(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (ToDoListWindow::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&ToDoListWindow::delete_project_button)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (ToDoListWindow::*)(const QString & , const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&ToDoListWindow::edit_project_button)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject ToDoListWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_ToDoListWindow.offsetsAndSize,
+    qt_meta_data_ToDoListWindow,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_ToDoListWindow_t
+, QtPrivate::TypeAndForceComplete<ToDoListWindow, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QString &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *ToDoListWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ToDoListWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ToDoListWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ToDoListWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 14)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 14;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 14)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 14;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void ToDoListWindow::delete_project_button(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void ToDoListWindow::edit_project_button(const QString & _t1, const QString & _t2)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
