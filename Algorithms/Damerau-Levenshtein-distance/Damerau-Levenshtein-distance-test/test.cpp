121a7cca0a9544fa9c68fa8a37f4e9ef8052cbba
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2021-11-23 00:23:46 +0200

    Algorithms (previous too). Fixed tests. Lab is finshed

--- Algorithms/Damerau-Levenshtein-distance/Damerau-Levenshtein-distance-test/test.cpp
+++ Algorithms/Damerau-Levenshtein-distance/Damerau-Levenshtein-distance-test/test.cpp
@@ -5,35 +5,64 @@
 TEST(Damerau_Levenshtein_Non_Restricted_Distance, Different) {
 	// from wiki
 	std::string s1 = "ca", s2 = "abc";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 2);
+	std::vector<std::string> edit_sequence, edited_strings;
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 2);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 	// from internet
 	s1 = "gifts"; s2 = "profit";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 5);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 5);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 	// mine
 	// substitution
 	s1 = "aabc"; s2 = "babc";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 1);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 1);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 	// substitution and deletion
 	s1 = "aabc"; s2 = "bab";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 2);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 2);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 	// substitution and insert
 	s1 = "aab"; s2 = "babc";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 2);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 2);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 	// substitution and transposition
 	s1 = "aabc"; s2 = "bacb";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 2);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 2);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	// internal insertion
+	s1 = "aaaa"; s2 = "aaaba";
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 1);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	// right insertion
+	s1 = "aaaa"; s2 = "aaaab";
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 1);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	// left insertion
+	s1 = "baaa"; s2 = "abaaa";
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 1);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	// delete between and transpose
+	s1 = "ab"; s2 = "bccca";
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 4);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 }
 
 TEST(Damerau_Levenshtein_Non_Restricted_Distance, Same) {
+	std::vector<std::string> edit_sequence, edited_strings;
 	std::string s1 = "abc", s2 = "abc";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 0);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 0);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 }
 
 TEST(Damerau_Levenshtein_Non_Restricted_Distance, Empty) {
+	std::vector<std::string> edit_sequence, edited_strings;
 	std::string s1 = "", s2 = "abc";
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s2), 3);
-	EXPECT_EQ(damerau_levenshtein_dist(s2, s1), 3);
-	EXPECT_EQ(damerau_levenshtein_dist(s1, s1), 0);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s2, edit_sequence, edited_strings), 3);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	EXPECT_EQ(damerau_levenshtein_dist(s2, s1, edit_sequence, edited_strings), 3);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s1);
+	EXPECT_EQ(damerau_levenshtein_dist(s1, s1, edit_sequence, edited_strings), 0);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s1);
 }
 
 TEST(OSA_Distance, Different) {
@@ -74,6 +103,10 @@ TEST(OSA_Distance, Different) {
 	// left insertion
 	s1 = "baaa"; s2 = "abaaa";
 	EXPECT_EQ(osa_dist(s1, s2, edit_sequence, edited_strings), 1);
+	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
+	// more than non-restricted
+	s1 = "ab"; s2 = "bccca";
+	EXPECT_EQ(osa_dist(s1, s2, edit_sequence, edited_strings), 5);
 	EXPECT_EQ(edited_strings[edited_strings.size() - 1], s2);
 }
 
